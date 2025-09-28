a2502362ce5725242627d7671d3a27ea94f60df1
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-09-28 14:17:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ async function scrapeUrl(queryParams) {
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
         '--no-zygote',
